d7a116603dba66640d84d314e13dfca2b7f470e2
Mabel Christina <maybelchristina@gmail.com>
2021-10-28 14:36:15 +0530

    [Mabel] Add: UC1 added

---
+++ ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+
+        public string toString()
+        {
+            return $"ProductID: - {ProductID}  UserID: - { UserID} Rating:- {Rating} Review:- { Review}  isLike:- { isLike}";
+        }
+    }
+}
